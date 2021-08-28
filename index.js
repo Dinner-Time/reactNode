4b07a1ce98ee93b03e2966af5429470f91c88924
A.A.W <wpdlxldpf4@gmail.com>
2021-08-28 23:58:01 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://dinner:time@reactnode.n2sb8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=>{
+    console.log('connected')
+}).catch((err) =>{
+    console.log(err)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
